c39098f80415db49eb157c9da5b7c93c37a7a928
David Klein <dave@smokinheads.de>
2017-08-19 01:26:50 +0200

    fix flash

--- kha/filesystem/air/FileStreamBase.hx
+++ kha/filesystem/air/FileStreamBase.hx
@@ -3,7 +3,7 @@ package kha.filesystem.air;
 import flash.filesystem.FileStream;
 
 class FileStreamBase extends FileStream {
-	public function new( file: FileBase ) {
+	public function new( file: FileDescriptorBase ) {
 		super();
 		open(file, flash.filesystem.FileMode.WRITE);
 	}
