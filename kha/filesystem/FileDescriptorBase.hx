5967a45a614ac198e04707d3a971b4516611c9cf
David Klein <dave@smokinheads.de>
2017-10-20 23:04:25 +0200

    default to stubs

--- kha/filesystem/FileDescriptorBase.hx
+++ kha/filesystem/FileDescriptorBase.hx
@@ -3,30 +3,22 @@ package kha.filesystem;
 // TODO (DK) handle via include paths (kha backend style)
 #if flash
 typedef FileDescriptorBase = kha.filesystem.air.FileDescriptorBase;
-#end
-
-#if kha_debug_html5
+#elseif kha_debug_html5
 typedef FileDescriptorBase = kha.filesystem.debug_html5.FileDescriptorBase;
-#end
-
-#if kha_html5
+#elseif kha_html5
 typedef FileDescriptorBase = kha.filesystem.html5.FileDescriptorBase;
-#end
-
-#if kha_kore
+#elseif kha_kore
 typedef FileDescriptorBase = kha.filesystem.kore.FileDescriptorBase;
-#end
-
-#if kha_krom
+#else
 class FileDescriptorBase {
-	public function new( ?url: String ) {		
+	public function new( ?url: String ) {
 	}
 
-	public function _browseForOpen( title: String, then: String -> Void ) {		
+	public function _browseForOpen( title: String, then: String -> Void ) {
 	}
 
-	public function _browseForSave( title: String, then: String -> Void ) {		
+	public function _browseForSave( title: String, then: String -> Void ) {
 	}
-	
+
 }
 #end
--- kha/filesystem/FileStreamBase.hx
+++ kha/filesystem/FileStreamBase.hx
@@ -3,29 +3,21 @@ package kha.filesystem;
 // TODO (DK) handle via include paths (kha backend style)
 #if flash
 typedef FileStreamBase = kha.filesystem.air.FileStreamBase;
-#end
-
-#if kha_debug_html5
+#elseif kha_debug_html5
 typedef FileStreamBase = kha.filesystem.debug_html5.FileStreamBase;
-#end
-
-#if kha_html5
+#elseif kha_html5
 typedef FileStreamBase = kha.filesystem.html5.FileStreamBase;
-#end
-
-#if kha_kore
+#elseif kha_kore
 typedef FileStreamBase = kha.filesystem.kore.FileStreamBase;
-#end
-
-#if kha_krom
+#else
 class FileStreamBase {
 	public function new( fd: FileDescriptorBase ) {
 	}
 
-	public function writeString( s: String ) {		
+	public function writeString( s: String ) {
 	}
 
-	public function close() {		
+	public function close() {
 	}
 }
 #end
